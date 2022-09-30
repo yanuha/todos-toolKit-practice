c0f047b12cdf26c4e80767436f3777be94a9fc3c
Evgeniy Yanushko <yanuha2010@mail.ru>
2022-09-30 18:10:46 +0300

    redux persist settings

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
-import { Provider } from "react-redux";
-import ReactDOM from "react-dom";
-import { store } from "./store";
+import { Provider } from 'react-redux';
+import ReactDOM from 'react-dom';
+import { persistor, store } from './store';
+import { PersistGate } from 'redux-persist/integration/react';
 
-import App from "./App";
+import App from './App';
 
-const rootElement = document.getElementById("root");
+const rootElement = document.getElementById('root');
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>,
   rootElement
 );
--- src/store.js
+++ src/store.js
@@ -1,12 +1,41 @@
+import { combineReducers } from 'redux';
 import { configureStore } from '@reduxjs/toolkit';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 import { todosReducer } from './features/Todos/todo-slices';
 import { filtersReducer } from './features/Filters/filter-slice';
 
+const rootReducer = combineReducers({
+  todos: todosReducer,
+  filters: filtersReducer,
+});
+
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 export const store = configureStore({
-  reducer: {
-    todos: todosReducer,
-    filters: filtersReducer,
-  },
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
   devTools: true,
 });
+
+export const persistor = persistStore(store);
